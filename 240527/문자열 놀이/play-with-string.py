f9e468efb2e83cd323b424c1220c321417976a65
oh183 <55764772+oh183@users.noreply.github.com>
2024-05-27 23:58:46 +0900

    Title: 문자열 놀이, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240527/문자열 놀이/play-with-string.py
@@ -0,0 +1,31 @@
+string, num_q = map(str, input().split())
+
+questions = [
+    input().replace(" ", "") for _ in range(int(num_q))
+]
+
+# Replace operation
+result = list(string)
+
+for question in range(int(num_q)):
+    if questions[question][0] == '1':
+        # swap a th <-> b th
+        a = int(questions[question][1]) - 1
+        b = int(questions[question][2]) - 1
+        temp = result[a]
+        result[a] = result[b]
+        result[b] = temp
+        print(''.join(result))
+    
+    if questions[question][0] == '2': 
+        # replace all a to b
+        a = questions[question][1]
+        b = questions[question][2]
+        
+        # result is already a list 
+        newArr = ''.join(result)
+        newArr = newArr.replace(a,b)
+        print(newArr)
+
+        # update the result 
+        result = list(newArr)

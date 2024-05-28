f156540daa96a7668d95c8bd72d1abb6cf5f7fe2
oh183 <55764772+oh183@users.noreply.github.com>
2024-05-28 15:38:52 +0900

    Title: 문자 제거하기, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240528/문자 제거하기/del-char.py
+++ 240528/문자 제거하기/del-char.py
@@ -4,12 +4,12 @@ length = len(string)
 
 currStr = list(string)
 
-while length != 1:
+while length > 1:
     # get the new line of input
     command = int(input())
     
     # execute the command 
-    if command > length:
+    if command >= length:
         currStr.pop()
     else:
         currStr.pop(command)

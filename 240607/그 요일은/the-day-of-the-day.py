b579d11a8f6f352d8716f18c2bcb46647d879469
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-07 22:19:05 +0900

    Title: 그 요일은, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240607/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,30 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+target = str(input())
+
+def daycalc(month):
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        max_month = 31
+    elif month == 2:
+        max_month = 29
+    else:
+        max_month = 30 
+    return max_month
+
+ctr = 1
+# d1 is monday
+while True:
+    max_month = daycalc(m1)
+    if m1 > m2:
+        break
+    
+    if m1 == m2 and d1 + 7 > d2:
+        break
+
+    d1 += 7
+    ctr += 1
+
+    if d1 > max_month: 
+        m1 += 1
+        d1 = d1 - max_month
+
+print(ctr)

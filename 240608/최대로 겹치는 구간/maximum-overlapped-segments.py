441584355fdde8b216dff91e6de0b0dc08be1e7b
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-08 02:42:39 +0900

    Title: 최대로 겹치는 구간, Time: 123ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240608/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,29 @@
+n = int(input())
+commands = [
+    tuple(input().split()) for _ in range(n)
+]
+
+checklist = [0] * 200
+
+for x1, x2 in commands:
+    start = int(x1)
+    end = int(x2)
+    while start <= end:
+        checklist[start] += 1
+        start += 1
+
+result = {}
+
+for el in checklist:
+    if el not in result:
+        result[el] = 1
+    else:
+        result[el] += 1
+
+maxkey = 0
+
+for key,val in result.items():
+    if maxkey < key and val > 1:
+        maxkey = key
+
+print(maxkey)

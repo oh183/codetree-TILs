00de9577f765add994f500b4f1bcadb57d87a495
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-08 20:53:43 +0900

    Title: 왔다 갔던 구역 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240608/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240608/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,46 +1,37 @@
-OFFSET = 1000
-MAX_R = 2000
+offset = 1000
+maxRange = 2 * offset
+checkList = [0] * (maxRange + 1)
 
-# 변수 선언 및 입력
+# Get input
 n = int(input())
-segments = []
+commands = [
+    tuple(map(str, input().split())) for _ in range(n)
+]
 
-# 현재 위치
-cur = 0
+# starting Pt
+starting = 1000
 
-for _ in range(n):
-	distance, direction = tuple(input().split())
-	distance = int(distance)
-	
-	if direction == 'L':
-		# 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
-		section_left = cur - distance
-		section_right = cur
-		cur -= distance
-	else:
-		# 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
-		section_left = cur
-		section_right = cur + distance
-		cur += distance
-	
-	segments.append([section_left, section_right])
+# Fill the checklist
+for x1, x2 in commands:
+    x1 = int(x1)
 
-	
-checked = [0] * (MAX_R + 1)
+    if x2 == "R":
+        # going right
+        start = starting
+        end = start + x1
+        starting += x1
+    else:
+        # going left
+        start = starting - x1 
+        end = starting
+        starting -= x1 
+    
+    for i in range(start, end):
+        checkList[i] += 1
 
-for x1, x2 in segments:
-	# OFFSET을 더해줍니다.
-	x1, x2 = x1 + OFFSET, x2 + OFFSET
-	
-	# 구간을 칠해줍니다.
-	# 구간 단위로 진행하는 문제이므로
-	# x2에 등호가 들어가지 않음에 유의합니다.
-	for i in range(x1, x2):
-		checked[i] += 1
+ctr = 0
+for i in checkList:
+    if i > 1:
+        ctr += 1
 
-# 2번 이상 지나간 영역의 크기를 구합니다.
-cnt = 0
-for elem in checked:
-	if elem >= 2:
-		cnt += 1
-print(cnt)
+print(ctr)

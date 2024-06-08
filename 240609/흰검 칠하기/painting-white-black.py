d5fb4b6c8b8c4723fa0f6ffe7888a1cf18ebd192
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-09 01:46:11 +0900

    Title: 흰검 칠하기, Time: 226ms, Memory: 30MB, Status: Passed - CodeTree

--- 240609/흰검 칠하기/painting-white-black.py
+++ 240609/흰검 칠하기/painting-white-black.py
@@ -1,83 +1,44 @@
-offset = 100000
-maxRange = 2 * offset
-checkList = [0] * (maxRange + 1)
-starting = offset
-prevStart = starting
-prevDirec = ""
-prevX1 = 0
-
-# input
+MAX_K = 100000
+# 변수 선언 및 입력:
 n = int(input())
-commands = [
-    tuple(input().split()) for _ in range(n)
-]
+a = [0] * (2 * MAX_K + 1)
+cnt_b = [0] * (2 * MAX_K + 1)
+cnt_w = [0] * (2 * MAX_K + 1)
+b, w, g = 0, 0, 0
 
+cur = MAX_K
+for _ in range(n):
+    x, c = tuple(input().split())
+    x = int(x)
 
-# fill in the checklist
-for x1, x2 in commands:
-    x1 = int(x1)
+    if c == 'L':
+        # x칸 왼쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 1
+            cnt_w[cur] += 1
+            x -= 1
 
-    if x1 == 1:
-        if prevX1 > 1:
-            starting -= 1
-        if x2 == 'R':
-            if checkList[starting] == 0:
-                checkList[starting] = 'B'
-            else: 
-                checkList[starting] += 'B'
-        else:
-            if checkList[starting] == 0:
-                checkList[starting] = 'W'
-            else: 
-                checkList[starting] += 'W'
-        continue
-
-    if x2 == 'R':
-        start = starting
-        end = start + x1
-        prevStart = starting
-        starting += x1
-        for i in range(start, end):
-            if checkList[i] == 0:
-                checkList[i] = 'B'
-            else:
-                checkList[i] += 'B'
-
+            if x: 
+                cur -= 1
     else:
-        start = starting - x1
-        end = starting
-        prevStart = starting
-        starting -= x1
-        for i in range(start, end):
-            if checkList[i] == 0:
-                checkList[i] = 'W'
-            else:
-                checkList[i] += 'W'
-    prevX1 = x1
+        # x칸 오른쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 2
+            cnt_b[cur] += 1
+            x -= 1
 
-blackCnt, whiteCnt, white, black, grey = 0, 0, 0, 0, 0
-lastColor = 0
+            if x: 
+                cur += 1
 
-for idx, val in enumerate(checkList):
-    i = val
-    if i != 0:
-        for j in i:
-            if j == 'B':
-                blackCnt += 1
-            else:
-                whiteCnt += 1
-            lastColor = j
+for i in range(2 * MAX_K + 1):
+    # 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
+    if cnt_b[i] >= 2 and cnt_w[i] >= 2: 
+        g += 1
+    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+    elif a[i] == 1: 
+        w += 1
+    elif a[i] == 2: 
+        b += 1
 
-        if blackCnt > 1 and whiteCnt > 1:
-            grey += 1
-            blackCnt, whiteCnt = 0, 0
-            continue
-        
-        if lastColor == 'B':
-            black += 1
-        else:
-            white += 1
-    
-    blackCnt, whiteCnt = 0, 0
-
-print(white, black, grey)
+# 정답을 출력합니다.
+print(w, b, g)

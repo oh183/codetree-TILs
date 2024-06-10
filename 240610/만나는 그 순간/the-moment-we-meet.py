a0a5a4f6986a366998f46b3452f532eb8229ae00
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-10 16:12:49 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240610/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,59 @@
+N, M = map(int, input().split())
+
+# A 부터 시뮬레이션
+offset = 1000
+MaxRange = 2 * offset
+a_sim = [0] * (MaxRange + 1)
+starting = offset
+
+timestamp = 0
+
+for _ in range(N):
+    d, t = input().split()
+    t = int(t)
+
+    if d == 'L':
+        while t > 0:
+            a_sim[starting] = timestamp
+            timestamp += 1
+            t -= 1
+
+            starting -= 1
+    else:
+        while t > 0:
+            a_sim[starting] = timestamp
+            timestamp += 1
+            t -= 1
+
+            starting += 1
+
+# B 시뮬레이션
+b_sim = [0] * (MaxRange + 1)
+starting = offset
+timestamp = 0
+
+for _ in range(M):
+    d, t = input().split()
+    t = int(t)
+
+    if d == 'L':
+        while t > 0:
+            b_sim[starting] = timestamp
+            timestamp += 1
+            t -= 1
+            starting -= 1
+    else:
+        while t > 0:
+            b_sim[starting] = timestamp
+            timestamp += 1
+            t -= 1
+            starting += 1
+
+# compare
+result = -1
+for i in range(990, len(a_sim)):
+    if a_sim[i] == b_sim[i] and a_sim[i] != 0:
+        result = a_sim[i]
+        break
+
+print(result)

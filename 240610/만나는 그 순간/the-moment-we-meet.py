18385452e22135fdbae69e25cd652e49d29d6ae3
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-10 17:19:34 +0900

    Title: 만나는 그 순간, Time: 138ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240610/만나는 그 순간/the-moment-we-meet.py
+++ 240610/만나는 그 순간/the-moment-we-meet.py
@@ -1,59 +1,56 @@
 N, M = map(int, input().split())
 
-# A 부터 시뮬레이션
-offset = 1000000
-MaxRange = 2 * offset
-a_sim = [0] * (MaxRange + 1)
-starting = offset
+### Simulation -> Index = time, value = location
 
-timestamp = 0
+### Simulation A
+sim_a = [0] * 1000001
+timeStamp = 0
+location = 0
 
 for _ in range(N):
     d, t = input().split()
     t = int(t)
 
-    if d == 'L':
+    if d == "L":
         while t > 0:
-            a_sim[starting] = timestamp
-            timestamp += 1
+            location -= 1
+            sim_a[timeStamp] = location
+            timeStamp += 1
             t -= 1
-
-            starting -= 1
     else:
         while t > 0:
-            a_sim[starting] = timestamp
-            timestamp += 1
+            location += 1
+            sim_a[timeStamp] = location
+            timeStamp += 1
             t -= 1
 
-            starting += 1
-
-# B 시뮬레이션
-b_sim = [0] * (MaxRange + 1)
-starting = offset
-timestamp = 0
+### Simulation B
+sim_b = [0] * 1000001
+timeStamp = 0
+location = 0
 
 for _ in range(M):
     d, t = input().split()
     t = int(t)
 
-    if d == 'L':
+    if d == "L":
         while t > 0:
-            b_sim[starting] = timestamp
-            timestamp += 1
+            location -= 1
+            sim_b[timeStamp] = location
+            timeStamp += 1
             t -= 1
-            starting -= 1
     else:
         while t > 0:
-            b_sim[starting] = timestamp
-            timestamp += 1
+            location += 1
+            sim_b[timeStamp] = location
+            timeStamp += 1
             t -= 1
-            starting += 1
 
-# compare
+# counting
 result = -1
-for i in range(990, len(a_sim)):
-    if a_sim[i] == b_sim[i] and a_sim[i] != 0:
-        result = a_sim[i]
+for i in range(len(sim_a)):
+    if sim_a[i] == sim_b[i] and sim_a != 0:
+        result = i + 1
         break
 
 print(result)

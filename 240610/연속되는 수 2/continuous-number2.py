f3b857c7d9c0ba095b66e72a47cc015347a09866
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-10 10:01:43 +0900

    Title: 연속되는 수 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240610/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+num_freq = [0] * 1001
+counter = 0
+
+for i in range(len(arr)):
+    if i == 0 or arr[i] == arr[i - 1]:
+        counter += 1
+        current_number = arr[i]
+
+    else:
+        if counter > num_freq[current_number]:
+            if i - 1 == 0:
+                num_freq[current_number] = counter
+                continue
+            
+            if counter > 0:
+                num_freq[current_number] = counter + 1
+
+        counter = 0
+
+print(max(num_freq))

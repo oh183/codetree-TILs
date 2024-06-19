d5ccc0e948ad90c84ca6a8a0cbda0f5699a56df8
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-20 03:47:45 +0900

    Title: 최고의 13위치, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240620/최고의 13위치/best-place-of-13.py
+++ 240620/최고의 13위치/best-place-of-13.py
@@ -6,7 +6,7 @@ grid = [
 
 coin = 0
 for r in range(n):
-    for c in range(0, n - 2, 3):
+    for c in range(0, n - 2):
         value = grid[r][c] + grid[r][c + 1] + grid[r][c+2]
         coin = max(coin, value)
 print(coin)

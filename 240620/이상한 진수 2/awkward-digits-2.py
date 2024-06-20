11f9d3abe38025a1a3da45d20dd4705b729f2ec7
oh183 <55764772+oh183@users.noreply.github.com>
2024-06-20 17:17:12 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240620/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,18 @@
+binary = input()
+
+def binary_to_decimal(number):
+    reversedString = number[::-1]
+    result = 0 
+
+    for idx, val in enumerate(reversedString):
+        result += int(val) * pow(2, idx)    
+    return result
+
+MaxVal = []
+
+for idx, val in enumerate(binary):
+    if val == '0':
+        tempNumber = binary[:idx] + '1' + binary[idx + 1:]
+        MaxVal.append(binary_to_decimal(tempNumber))
+
+print(max(MaxVal))

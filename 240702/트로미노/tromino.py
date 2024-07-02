7eb5fb31d1009b82ada51d5467758190dd1d848f
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-02 14:18:38 +0900

    Title: 트로미노, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240702/트로미노/tromino.py
+++ 240702/트로미노/tromino.py
@@ -7,7 +7,7 @@ grid = [
 def case_1():
     result = 0
     for i in range(n):
-        for j in range(n - 2):
+        for j in range(m - 2):
             cnt = grid[i][j] + grid[i][j + 1] + grid[i][j + 2]
             result = max(result, cnt)
     return result
@@ -15,7 +15,7 @@ def case_1():
 # case 2
 def case_2():
     result = 0
-    for j in range(n):
+    for j in range(m):
         for i in range(n - 2):
             cnt = grid[i][j] + grid[i + 1][j] + grid[i + 2][j]
             result = max(result, cnt)
@@ -25,7 +25,7 @@ def case_2():
 def case_3():
     result = 0
     for i in range(n - 1):
-        for j in range(n - 1):
+        for j in range(m - 1):
             cnt = grid[i][j] + grid[i + 1][j] + grid[i + 1][j + 1]
             result = max(result, cnt)
     return result
@@ -33,9 +33,8 @@ def case_3():
 # case 4
 def case_4():
     result = 0
-    result = 0
     for i in range(n - 1):
-        for j in range(n - 1):
+        for j in range(m - 1):
             cnt = grid[i][j] + grid[i][j + 1] + grid[i + 1][j]
             result = max(result, cnt)
     return result
@@ -44,7 +43,7 @@ def case_4():
 def case_5():
     result = 0
     for i in range(n - 1):
-        for j in range(n - 1):
+        for j in range(m - 1):
             cnt = grid[i][j] + grid[i][j + 1] + grid[i + 1][j + 1]
             result = max(result, cnt)
     return result
@@ -53,10 +52,10 @@ def case_5():
 def case_6():
     result = 0
     for i in range(n - 1):
-        for j in range(n - 1):
+        for j in range(m - 1):
             cnt = grid[i + 1][j] + grid[i][j + 1] + grid[i + 1][j + 1]
             result = max(result, cnt)
     return result
 
-total = max(case_1(),case_2(),case_3(),case_4(),case_5(),case_6())
+total = max(case_1(), case_2(), case_3(), case_4(), case_5(), case_6())
 print(total)

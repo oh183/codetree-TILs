aa790dc825832f88552a3cb4a7dff06bd28af714
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-02 12:51:41 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240702/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+grid = [
+    list(map(int, input().split())) for _ in range(n)
+]
+
+def is_happy(seq, m):
+    cnt = 1
+    
+    for i in range(1, n):
+        if seq[i - 1] == seq[i]:
+            cnt += 1
+        else:
+            cnt = 1
+        res = max(1, cnt)
+    return res >= m
+
+total = 0 
+for row in range(n):
+    total += is_happy(grid[row][:], m)
+
+
+vertical = []
+val = ""
+for col in range(n):
+    for row in range(n):
+        val += str(grid[row][col])
+    vertical.append(val)
+    val = ""
+
+for i in vertical:
+    total += is_happy(i, m)
+
+print(total)

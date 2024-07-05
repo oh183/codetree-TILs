f07ca310c3040ba4b30245db9bee4827f8f468a0
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-05 12:49:38 +0900

    Title: 1차원 젠가, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240705/1차원 젠가/jenga-1d.py
+++ 240705/1차원 젠가/jenga-1d.py
@@ -1,40 +1,36 @@
-# 블럭빼기
-
+# 변수 선언 및 입력
 n = int(input())
-Blocks = []
-for i in range(n):
-    Blocks.append(int(input()))
-
-commands = [ 
-    list(map(int, input().split())) for _ in range(2)
+numbers = [
+    int(input())
+    for _ in range(n)
 ]
-
-# remove block 
-def clean():
-    global Blocks
-    Blocks.reverse()
-    for start, end in commands:
-        for idx in range(start - 1, end):
-            Blocks[idx] = 0
-    Blocks.reverse()
-
-# update 
-def update():
-    global Blocks
-    temp = []
-    for i in Blocks:
-        if i > 0:
-            temp.append(i)
-    Blocks = temp
-
-# print
-def pprint():
-    for i in Blocks:
-        print(i)
+end_of_array = n
 
 
-for _ in range(len(commands)):
-    clean()
-    update()
-print(len(Blocks))
-pprint()
+# 입력 배열에서 지우고자 하는 부분 수열을 삭제합니다.
+def cut_array(start_idx, end_idx):
+    global end_of_array, numbers
+    
+    temp_arr = []
+    
+    # 구간 외의 부분만 temp 배열에 순서대로 저장합니다.
+    for i in range(end_of_array):
+        if i < start_idx or i > end_idx:
+            temp_arr.append(numbers[i])
+
+    # temp 배열을 다시 numbers 배열로 옮겨줍니다.
+    end_of_array = len(temp_arr)
+    for i in range(end_of_array):    
+        numbers[i] = temp_arr[i]
+
+
+# 두 번에 걸쳐 지우는 과정을 반복합니다.
+for _ in range(2):
+    s, e = tuple(map(int, input().split()))
+    # [s, e] 구간을 삭제합니다.
+    cut_array(s - 1, e - 1)
+
+# 출력:
+print(end_of_array)
+for i in range(end_of_array):
+    print(numbers[i])

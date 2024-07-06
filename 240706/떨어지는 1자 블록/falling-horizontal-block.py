a551be6a3862f895d1e4be98472667e023cd4f36
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-06 15:15:06 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240706/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,44 @@
+# input
+n, m, k = map(int, input().split())
+grid = [
+    list(map(int, input().split())) for _ in range(n)
+]
+
+# move_down 가능한지 보는 함수
+def canMove(new_x, start, end):
+    for i in range(start, end):
+        if grid[new_x][i] == 1:
+            return False
+    return True
+
+# move_down execute 함수
+def moveDown(r, start, end):
+    global grid
+    # remove the current rectangle at a given point
+    if r > 0:
+        for i in range(start, end + 1):
+            grid[r - 1][i] = 0
+    # move the rectangle
+        for i in range(start, end + 1):
+            grid[r][i] = 1
+    else:
+        # move the rectangle
+        for i in range(start, end + 1):
+            grid[r][i] = 1
+
+# 출력 함수
+def printGrid():
+    for i in range(n):
+        for j in range(n):
+            print(grid[i][j], end = " ")
+        print()
+
+start, end = k - 1, k + m - 1
+new_x = -1
+while True:
+    if canMove(new_x + 1, start, end - 1):
+        moveDown(new_x + 1, start, end - 1)
+    else:
+        printGrid()
+        break
+    new_x += 1

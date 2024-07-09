4d10d4297465f6497575eed1f0a1cfdf1b8144a1
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-09 22:37:57 +0900

    Title: 왕실의 기사 대결, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240709/왕실의 기사 대결/royal-knight-duel.py
+++ 240709/왕실의 기사 대결/royal-knight-duel.py
@@ -15,7 +15,7 @@ knightCommands = [
 # 기사 배열
 knight = [[0 for _ in range(L)] for _ in range(L)]
 gisa = 1
-knightHealth, knightLocation, knightDamage = {}, {}, {}
+knightHealth, knightLocation, knightDamage, knightStatus = {}, {}, {}, {}
 for command in knightCommands:
     r, c, h, w, k = command
     r, c, w, h = r - 1, c - 1, w, h
@@ -36,14 +36,50 @@ for command in knightCommands:
 
     # 받은 총 데미지
     if gisa not in knightDamage:
-        knightHealth[gisa] = 0
+        knightDamage[gisa] = 0
+
+    if gisa not in knightStatus:
+        knightStatus[gisa] = 1
 
     # 기사 번호 증가
     gisa += 1
 
+
 # range checker
-def in_range(i,j):
-    return 0 <= i < N and 0 <= j < N
+def in_range(i, j):
+    return 0 <= i < L and 0 <= j < L
+
+# canGo? -> 기사 안에서만 돌아다니고, 주어진 방향으로만 돌아다니기
+def canGo(x, y, direction, bfsDirection, currKnightNumber):
+    if direction == bfsDirection:
+        return in_range(x, y) and knight[x][y] != 0
+    else:
+        return in_range(x, y) and knight[x][y] != 0 and knight[x][y] == currKnightNumber
+
+# bfs
+def bfs(currR, currC, direction):
+    visitedKnights = set()
+    q = deque([(currR, currC)])
+    visited = [[0 for _ in range(L)] for _ in range(L)]
+    visited[currR][currC] = 1
+    dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+    while q:
+        x, y = q.popleft()
+        currKnightNumber = knight[x][y]
+        for i in range(4):
+            nx, ny = x + dxs[i], y + dys[i]
+            if canGo(nx, ny, direction, i, currKnightNumber) and not visited[nx][ny]:
+                q.append((nx, ny))
+                visited[nx][ny] = 1
+                visitedKnights.add(knight[nx][ny])
+    return list(visitedKnights)
+
+def deleteKnight(target):
+    r, c, h, w = knightLocation[target]
+    for row in range(r + h):
+        for col in range(c + w):
+            knight[row][col] = 0
+
 
 # 왕의 명령
 kingCommands = [list(map(int, input().split())) for _ in range(Q)]
@@ -55,78 +91,63 @@ for kingCommand in kingCommands:
 
     # (1) 기사 이동
     currR, currC, height, width = knightLocation[knightName]
+    if currR == -999:
+        continue
     next_row, next_col = currR + dxs[direction], currC + dys[direction]
 
-    # 해당 위치에 기사가 있는지 확인 (고치기)
-    while True:
-        counter, stop = 0, 0
-        for i in range(next_row, height + 1):
-            for j in range(next_col, width + 1):
-                if knight[i][j] > 0:
-                    if knight[i][j] == knightName:
-                        counter += 1
-                    else:
-                        # 서로 다른 기사인 경우
-                        # 기사 범위 찾기
-                        gisaName = knight[i][j]
-                        r, c, h, w = knightLocation[knightName]
-                        for anotherR in range(r, r + h):
-                            for anotherC in range(c, c + w):
-                                if chessBoard[anotherR][anotherC] == 2:
-                                    stop = 1
-                                    break
-                                else:
-                                    continue
-                            
+    # 현재 도형에서 1칸만큼 밀었을때 방문 가능했던 배열들
+    visitedKnights = bfs(currR, currC, direction)
 
+    isObstacle = 0
+    for knight in visitedKnights:
+        r, c, h, w = knightLocation[knight]
+        if r != -999:
+            newRow, newCol = r + dxs[direction], c + dys[direction]
+            for i in range(newRow, newRow + h):
+                for j in range(newCol, newCol + w):
+                    if not in_range(i, j) or chessBoard[i][j] == 2:
+                        isObstacle = 1
 
-                # 만약 벽이나, 범위를 벗어났다면?
-                if chessBoard[i][j] == 2 or not in_range(i,j):
-                    stop = 1
-                    break
-
-        if stop:
-            next_row, next_col = currR, currC
-            break
-
-        if counter > 0:
-            next_row += dxs[direction]
-            next_col += dys[direction]
-
-        else:
-            break
-
-    # 계산한 next_row, next_col 사용해서 이동
-    for i in range(next_row, currR - 1, -1):
-        for j in range(next_col, currC - 1, -1):
-            if knight[i - 1][j - 1] == 0:
-                continue
-            else:
-                # find the top left point
-                gisa = knight[i-1][j-1]
-                r, c, h, w = knightLocation[gisa]
-                newR, newC = r + dxs[direction], c + dys[direction]
-                knightLocation[gisa] = (newR, newC, h, w)
+    # 밀렸던 칸에 벽이 없었던 경우
+    if not isObstacle:
+        # 업데이트
+        for knight in visitedKnights:
+            r, c, h, w = knightLocation[knight]
+            if r != -999:
+                newRow, newCol = r + dxs[direction], c + dys[direction]
+                knightLocation[knight] = newRow, newCol, h, w
 
 
     # 업데이트 된 배열을 바탕으로 기사배열 초기화
     knight = [[0 for _ in range(L)] for _ in range(L)]
     for i in range(1, N + 1):
         r, c, h, w = knightLocation[i]
-        for j in range(r, h + 1):
-            for k in range(c, w + 1):
-                knight[j][k] = i
+        if r != -999:
+            for j in range(r, r + h):
+                for k in range(c, c + w):
+                    knight[j][k] = i
 
     # (2) 이동 후 대결 데미지 업데이트
     # 업데이트 된 Knight 배열을 순회하며 체력 업데이트
-    for i in range(L):
-        for j in range(L):
-            currKnight = knight[i][j]
-            if chessBoard[i][j] == 1 and currKnight != knightName:
-                knightHealth[currKnight] -= 1
-                knightDamage[currKnight] += 1
+    if not isObstacle:
+        for i in range(L):
+            for j in range(L):
+                currKnight = knight[i][j]
+                if currKnight in visitedKnights:
+                    if currKnight > 0:
+                        if chessBoard[i][j] == 1 and currKnight != knightName:
+                            knightHealth[currKnight] -= 1
+                            knightDamage[currKnight] += 1
+
+                        if knightHealth[currKnight] == 0:
+                            # 말이 죽은 경우
+                            deleteKnight(currKnight)
+                            knightLocation[currKnight] = (-999, -999, -999, -999)
+
 
 # 최종 결과 프린트
+res = 0
 for i in range(1, N + 1):
-    if knightHealth[i] > 0:
-        print(knightDamage[i], end = " ")
+    if knightHealth[i] > 0 and knightStatus[i] > 0:
+        res += knightDamage[i]
+print(res)

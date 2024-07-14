074751a237b4f21f1bccb8b47b94f16d6b9cfb6f
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-15 08:06:12 +0900

    Title: 싸움땅, Time: 195ms, Memory: 26MB, Status: Passed - CodeTree

--- 240715/싸움땅/battle-ground.py
+++ 240715/싸움땅/battle-ground.py
@@ -1,145 +1,90 @@
-# i love you
-# -stephanie-
+N, M, K = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+gun = [[[] for _ in range(N)] for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if arr[i][j]>0:
+            gun[i][j].append(arr[i][j])
 
-n, m, k = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [[0]*N for _ in range(N)]
+# [0]:i, [1]:j, [2]:dir, [3]:power,[4]:gun,[5]:score  방향 0,1,2,3 (↑, →, ↓, ←)
+players = {}            # 1~M 플레이어
+for m in range(1,M+1):
+    i,j,d,p = map(int, input().split())
+    players[m]=[i-1,j-1,d,p,0,0]
+    arr[i-1][j-1]=m
 
-# 플레이어 정보
-player = []
-for _ in range(m):
-    x, y, d, s = map(int, input().split())
-    x, y = x -1, y - 1
-    player.append((x, y, d, s))
+opp = {0:2, 1:3, 2:0, 3:1}
+#방향 상,우,하,좌
+di = [-1, 0, 1, 0]
+dj = [ 0, 1, 0,-1]
 
-# 스코어, 총 능력치 저장하는 어레이
-scoreBoard, gun = {}, {}
-for i in range(m):
-    if i not in scoreBoard:
-        scoreBoard[i] = 0
-        gun[i] = 0
+def leave(num,ci, cj, cd, cp, cg, cs):
+    for k in range(4):      # 현재방향부터 시계방향 90도씩 빈칸 찾기 (최소한 내가온(상대방이 온)칸은 비어있음)
+        ni,nj=ci+di[(cd+k)%4], cj+dj[(cd+k)%4]
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
+            # 총이 있다면 가장 강한총 획득
+            if len(gun[ni][nj])>0:
+                cg = max(gun[ni][nj])
+                gun[ni][nj].remove(cg)
+            arr[ni][nj]=num # 내 정보 갱신 후 리턴
+            players[num]=[ni,nj,(cd+k)%4,cp,cg,cs]
+            return
 
-# 플레이어 업데이트
-offset = 0
-playerArr = [[0 for _ in range(n)] for _ in range(n)]
-for playerInfo in player:
-    x, y, d, s = playerInfo
-    playerArr[x][y] = offset
-    offset += 1
+for _ in range(K):      # K라운드 동안 경기진행
+    # 1번~M번까지 전체 플레이어 번호순대로 처리
+    for i in players:
+        # [1] 한 칸 이동(격자 벗어나면 반대방향)
+        ci,cj,cd,cp,cg,cs = players[i]
+        ni,nj=ci+di[cd], cj+dj[cd]
+        if ni<0 or N<=ni or nj<0 or N<=nj:  # 범위를 벗어남
+            cd = opp[cd]                    # 방향 반대로
+            ni,nj=ci+di[cd], cj+dj[cd]      # 한 칸 이동
+        arr[ci][cj]=0                       # 이전위치에서 플레이어 표시 제거
 
-
-# range checker
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
-
-def movePlayer(playerNum, x, y, d, s, dx, dy):
-    # 이동 후 플레이어의 여부에 따라 2가지 경우
-    # 플레이어가 있는 경우
-    if playerArr[x][y] > 0:
-        # 상대방의 능력
-        oppNumber = playerArr[x][y]
-        oppX, oppY, oppD, oppS = player[oppNumber]
-        oppPower = gun[oppNumber] + oppS
-
-        # 나의 능력
-        myPower = gun[playerNum] + s
-
-        # 능력치 비교
-        if myPower > oppPower:
-            # 내 능력치가 더 큰 경우
-
-            # 이긴 플레이어
-            difference = myPower - oppPower
-            scoreBoard[playerNum] += difference
-            playerArr[x][y] = playerNum
-            player[playerNum] = (x, y, d, s)
-
-            # 진 플레이어
-            # 해당 셀에 총을 내려놓고, 내 총을 버렸다 처리
-            arr[x][y] = max(arr[x][y], gun[oppNumber])
-            gun[oppNumber] = 0
-
-            # 다음 방향으로 이동
-            nx, ny = x + dx[d], y + dy[d]
-
-            # 방향 찾아주기
-            while not in_range(nx, ny) or arr[nx][ny] - 100001 >= 0:
-                d = (d + 1) % 4
-
-            # 이동
-            nx, ny = nx + dx[d], y + dy[d]
-
-            # 총이 있는지 확인
-            if arr[nx][ny] > 0:
-                # 총이 있습니다
-                gun[oppNumber] = arr[nx][ny]
-
-            playerArr[nx][ny] = oppNumber
-            player[playerNum] = (nx, ny, d, s)
-
+        # [2-1] 이동한 위치가 빈칸인 경우 => 강한총 획득
+        if arr[ni][nj]==0:
+            if len(gun[ni][nj])>0:          # 총이 있는 경우
+                mx = max(gun[ni][nj])
+                if cg<mx:                   # 더 강한총이면 교체
+                    if cg>0:                # 총이 있는 경우
+                        gun[ni][nj].append(cg)  # 내총을 바닥에..반납
+                    gun[ni][nj].remove(mx)
+                    cg=mx
+            arr[ni][nj]=i                   # 위치이동
+            players[i]=[ni,nj,cd,cp,cg,cs]  # 정보갱신
+        # [2-2] 빈칸아닌경우 => 싸울 상대방이 있는 경우
         else:
-            # 상대방의 능력치가 더 큰 경우
-            difference = oppPower - myPower
-            scoreBoard[oppNumber] += difference
+            enemy=arr[ni][nj]               # 상대방 번호 확인
+            ei,ej,ed,ep,eg,es = players[enemy]
+            if cp+cg>ep+eg or (cp+cg==ep+eg and cp>ep): # 내가 이기는 경우
+                cs+=(cp+cg)-(ep+eg)         # 공격력 차이만큼 점수 획득
+                leave(enemy,ni,nj,ed,ep,0,es)   # 상대방은 총을 놓고 떠남
 
-            # 해당 셀에 총을 내려놓고, 내 총을 버렸다 처리
-            arr[x][y] = max(arr[x][y], gun[playerNum])
-            gun[playerNum] = 0
+                # 이긴 플레이어는 가장 강한 총 얻기: 상대방총 vs 내총
+                if cg<eg:
+                    if cg>0:
+                        gun[ni][nj].append(cg)
+                    cg=eg
+                else:
+                    if eg>0:
+                        gun[ni][nj].append(eg)
+                arr[ni][nj]=i
+                players[i]=[ni,nj,cd,cp,cg,cs]
+            else:                                       # 지는 경우
+                es+=(ep+eg)-(cp+cg)         # 공격력 차이만큼 점수 획득
+                leave(i,ni,nj,cd,cp,0,cs)   # 내가 총 놓고 떠남
 
-            # 다음 방향으로 이동
-            nx, ny = x + dx[d], y + dy[d]
+                # 이긴 플레이어는 가장 강한 총 얻기: 상대방총 vs 내총
+                if eg<cg:
+                    if eg>0:
+                        gun[ni][nj].append(eg)
+                    eg=cg
+                else:
+                    if cg>0:
+                        gun[ni][nj].append(cg)
+                arr[ni][nj]=enemy
+                players[enemy]=[ni,nj,ed,ep,eg,es]
 
-            # 방향 찾아주기
-            while not in_range(nx, ny) or arr[nx][ny] - 100001 >= 0:
-                d = (d + 1) % 4
-
-            # 이동
-            nx, ny = nx + dx[d], y + dy[d]
-
-            # 총이 있는지 확인
-            if arr[nx][ny] > 0:
-                # 총이 있습니다
-                gun[playerNum] = arr[nx][ny]
-
-            arr[nx][ny] = 100001 + playerNum
-            player[playerNum] = (nx, ny, d, s)
-
-    else:
-        # 플레이어가 없는 경우
-        currentGun = gun[playerNum]
-        newGun = arr[x][y]
-
-        if currentGun < newGun:
-            arr[x][y] = currentGun
-            gun[playerNum] = newGun
-
-        # 플레이어 이동
-        arr[x][y] = 100001 + playerNum
-        player[playerNum] = (x, y, d, s)
-
-
-
-for round in range(1, k+1):
-    # 총 k 턴 만큼 반복하면서 시뮬레이션
-
-    for playerNumber in range(m):
-    # 각 플레이어가 본인의 방향대로 1 칸 이동
-        x, y, d, s = player[playerNumber]
-        dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
-        nx, ny = x + dx[d], y + dy[d]
-
-        if in_range(nx, ny):
-        # 격자를 벗어나지 않는 경우
-            playerArr[x][y] = 0
-            movePlayer(playerNumber, nx, ny, d, s, dx, dy)
-        else:
-        # 격자를 벗어나는 경우 -> 방향이 정 반대로 바뀜 (direction = (direction + 2) % 4, player 배열에도 업데이트
-            playerArr[x][y] = 0
-            d = (d + 2) % 4
-            # 방향 바꾼거 업데이트 해주고
-            nx, ny = x + dx[d], y + dy[d]
-            player[playerNumber] = (nx, ny, d, s)
-            # 해당 방향으로 이동
-            movePlayer(playerNumber, nx, ny, d, s, dx, dy)
-
-
-print(scoreBoard)
+for i in players:
+    print(players[i][5],end=' ')    # 각 플레이어 점수출력

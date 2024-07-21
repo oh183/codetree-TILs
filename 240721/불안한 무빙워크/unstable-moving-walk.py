c84f7c629422e7a3ed5637de1dfb681112ab8eb3
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-21 16:18:06 +0900

    Title: 불안한 무빙워크, Time: 480ms, Memory: 28MB, Status: Passed - CodeTree

--- 240721/불안한 무빙워크/unstable-moving-walk.py
+++ 240721/불안한 무빙워크/unstable-moving-walk.py
@@ -10,12 +10,19 @@ while stability.count(0) < k:
     stability.rotate(1)
     person.rotate(1)
 
+    if person[n - 1]:
+        person[n-1] = 0
+        
+
     # 가장 먼저 무빙워크에 올라간 사람부터 무빙워크가 회전하는 방향으로 한 칸 이동할 수 있으면 이동합니다.
     for idx in range(n - 1, -1, -1):
         if stability[idx] > 0 and person[idx] == 0 and person[idx - 1] == 1:
             person[idx] = 1
             person[idx - 1] = 0
             stability[idx] = max(0, stability[idx] - 1)
+    
+    if person[n - 1]:
+        person[n - 1] = 0
 
     # 1번 칸에 사람이 없고 안정성이 0이 아니라면 사람을 한 명 더 올립니다.
     if person[0] == 0 and stability[0] > 0:

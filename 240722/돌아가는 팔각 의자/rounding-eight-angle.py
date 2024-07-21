7533f9b2e6f8636456d4544c14156ce767cd4cfc
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-22 03:21:21 +0900

    Title: 돌아가는 팔각 의자, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/돌아가는 팔각 의자/rounding-eight-angle.py
+++ 240722/돌아가는 팔각 의자/rounding-eight-angle.py
@@ -1,67 +1,39 @@
-# 돌아가는 팔각의자
 from collections import deque
 
-totalList = [[] for _ in range(4)]
-for i in range(4):
-    mylist = input()
-    for j in range(len(mylist)):
-        totalList[i].append(int(mylist[j]))
+MAX=4
 
-first_chair = deque(totalList[0])
-second_chair = deque(totalList[1])
-third_chair = deque(totalList[2])
-fourth_chair = deque(totalList[3])
-chairs = [first_chair, second_chair, third_chair, fourth_chair]
-rotate_num = int(input())
-rotateCommand = [list(map(int, input().split())) for _ in range(rotate_num)]
+a=[None]
+for _ in range(MAX):
+    data=list(str(input()))
+    q=deque()
+    for ele in data:
+        q.append(int(ele))
+    a.append(q)
 
-def in_range(n):
-    return 0 <= n < 4
+def process(n,dir):
 
-for i in range(rotate_num):
-    n, d = rotateCommand[i]
-    n -= 1
+    # n번째로부터 왼쪽, 오른쪽으로 회전 대상 탐색
+    d=[None]+[0]*MAX # 의자마다 회전할 방향 저장
+    d[n]=dir
+    # 왼쪽 순서대로 탐색
+    for i in range(n,1,-1):
+        if a[i][6]!=a[i-1][2]:
+            d[i-1]=-d[i]
+    # 오른쪽 순서대로 탐색
+    for i in range(n,MAX):
+        if a[i][2]!=a[i+1][6]:
+            d[i+1]=-d[i]
 
-    sa, sb = 2, 6
-    rotateArr = [0, 0, 0, 0]
+    for i in range(1,MAX+1):
+        dir=d[i]
+        if dir==0:
+            continue
+        a[i].rotate(dir)
 
-    for i in range(4):
-        if i == 0:
-            if chairs[i][sa] != chairs[i + 1][sb]:
-                rotateArr[i] = d
-        elif i == 1 or i == 2:
-            if chairs[i][sa] != chairs[i + 1][sb]:
-                rotateArr[i] = d
-        else:
-            if chairs[i][sb] != chairs[i - 1][sa]:
-                rotateArr[i] = d
+K=int(input())
+for _ in range(K):
+    n,d=map(int,input().split())
+    process(n,d)
 
-    rotateArr[n] = d
-
-    if in_range(n - 1):
-        rotateArr[n - 1] *= -1
-
-    if in_range(n + 1):
-        rotateArr[n + 1] *= -1
-
-    if in_range(n - 1):
-        if rotateArr[n - 1] == 0:
-            for i in range(n):
-                rotateArr[i] = 0
-    if in_range(n + 1):
-        if rotateArr[n + 1] == 0:
-            for i in range(n+1, 4):
-                rotateArr[i] = 0
-
-    for idx, rot in enumerate(rotateArr):
-        chairs[idx].rotate(rot)
-
-s_arr = [0,0,0,0]
-for i in range(4):
-    if chairs[i][0] == 1:
-        s_arr[i] = 1
-
-
-
-
-print(s_arr[0] + 2 * s_arr[1] + 4 * s_arr[2] + 8 * s_arr[3])
+ans=a[1][0]*1+a[2][0]*2+a[3][0]*4+a[4][0]*8
+print(ans)

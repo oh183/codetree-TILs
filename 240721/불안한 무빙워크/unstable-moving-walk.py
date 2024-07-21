c250f796ec347a7ce16b722a200c25bcba3a7fe7
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-21 16:16:11 +0900

    Title: 불안한 무빙워크, Time: 5080ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

---
+++ 240721/불안한 무빙워크/unstable-moving-walk.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+n, k = map(int, input().split())
+stability = deque(list(map(int, input().split())))
+person = deque([0 for _ in range(2 * n)])
+
+trialCnt = 0
+while stability.count(0) < k:
+    # 무빙워크가 한 칸 회전합니다.
+    stability.rotate(1)
+    person.rotate(1)
+
+    # 가장 먼저 무빙워크에 올라간 사람부터 무빙워크가 회전하는 방향으로 한 칸 이동할 수 있으면 이동합니다.
+    for idx in range(n - 1, -1, -1):
+        if stability[idx] > 0 and person[idx] == 0 and person[idx - 1] == 1:
+            person[idx] = 1
+            person[idx - 1] = 0
+            stability[idx] = max(0, stability[idx] - 1)
+
+    # 1번 칸에 사람이 없고 안정성이 0이 아니라면 사람을 한 명 더 올립니다.
+    if person[0] == 0 and stability[0] > 0:
+        person[0] = 1
+        stability[0] = max(0, stability[0] - 1)
+
+    trialCnt += 1
+
+print(trialCnt)

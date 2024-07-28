5a0e6acaecf3eac9a108da564ed9c0d879172051
oh183 <55764772+oh183@users.noreply.github.com>
2024-07-29 01:32:23 +0900

    Title: 정육면체 한번 더 굴리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240729/정육면체 한번 더 굴리기/cube-rounding-again.py
@@ -0,0 +1,64 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+n1, n2, n3, n4, n5, n6 = 1,2,3,4,5,6
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+from collections import deque
+def in_range(nx, ny):
+    return 0<=nx<n and 0<=ny<n
+def bfs(si, sj):
+    value = grid[si][sj]
+    q = deque([(si, sj)])
+    visited = [[0 for _ in range(n)] for _ in range(n)]
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if in_range(nx,ny) and visited[nx][ny] == 0 and grid[nx][ny] == value:
+                q.append((nx, ny))
+                visited[nx][ny] = 1
+    res = 0
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == 1:
+                res += 1
+    return res
+
+    
+
+x, y = 0, 0
+dr = 0
+result = 0
+for turn in range(m):
+    nx, ny = x + dxs[dr], y + dys[dr]
+    score = 0
+    if 0 <= nx < n and 0 <= ny < n:
+        point = grid[nx][ny]
+        score = bfs(nx, ny)
+    else:
+        dr = (dr + 2) % 4
+        nx, ny = x + dx[dr], y + dy[dr]
+        point = grid[nx][ny]
+        score = bfs(nx, ny)
+    
+    result += (point * score)
+
+    # 면 바꾸기
+    if dr == 0:
+        n1,n3,n4,n6 = n4,n1,n6,n3
+    elif dr == 1:
+        n1,n2,n5,n6 = n2,n6,n1,n5
+    elif dr == 2:
+        n1,n3,n4,n6 = n3,n6,n1,n4
+    elif dr == 3:
+        n1,n2,n5,n6 = n5,n1,n6,n2
+    
+    # 방향 전환?
+    if n1 > grid[nx][ny]:
+        dr = (dr + 1) % 4
+    
+    x, y = nx, ny
+print(result)
